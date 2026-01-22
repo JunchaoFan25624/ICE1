6d9d58f443d5ab553e456a8938d1d3d0817d0eaf
JunchaoFan25624 <fanjun@sheridancollege.ca>
2026-01-21 19:54:51 -0500

    CardTrickTASK6.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -32,30 +32,43 @@ public class CardTrick {
             System.out.println(c.getSuit()+ " " +c.getValue());
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a card value (1-13): ");
-        int userValue =in.nextInt();
-        System.out.println("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=SPADES, 3=Clubs): ");
-        int userSuit =in.nextInt();
+        // //insert code to ask the user for Card value and suit, create their card
+        // // and search magicHand here
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("Enter a card value (1-13): ");
+        // int userValue =in.nextInt();
+        // System.out.println("Enter a suit (0-3 where 0=Hearts, 1=Diamonds, 2=SPADES, 3=Clubs): ");
+        // int userSuit =in.nextInt();
         
-        Card userCard = new Card();
-        userCard.setValue(userValue);
-        userCard.setSuit(Card.SUITS[userSuit]);
+        // Card userCard = new Card();
+        // userCard.setValue(userValue);
+        // userCard.setSuit(Card.SUITS[userSuit]);
+        // boolean result = false;
+        // for (Card card:magicHand) {
+        //     if (userCard.equals(card)) {
+        //         result = true;
+        //     }
+        // }
+        // //Then report the result here
+        // if (result) {
+        //     System.out.println("Nice! your card is in the magic hand.");
+        // } else {
+        //     System.out.println("Sorry, your card is not in the magic hand.");
+        // }
+        // add one luckcard hard code 2,clubs
+        Card luckyCard = new Card("Clubs",2);
         boolean result = false;
         for (Card card:magicHand) {
-            if (userCard.equals(card)) {
+            if (luckyCard.equals(card)) {
                 result = true;
             }
         }
-        //Then report the result here
         if (result) {
-            System.out.println("Nice! your card is in the magic hand.");
+            System.out.println("Nice! Lucky card is in the magic hand.");
         } else {
-            System.out.println("Sorry, your card is not in the magic hand.");
+            System.out.println("Sorry, Lucky card is not in the magic hand.");
         }
-        // add one luckcard hard code 2,clubs
+        
     }
     
 }
